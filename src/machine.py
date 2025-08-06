af6c874d4546fe3499f75994c243ecf3f3b35a0a
Maor <60894008+MaorIdi@users.noreply.github.com>
2025-08-06 19:09:46 +0300

    Update machine.py

--- src/machine.py
+++ src/machine.py
@@ -1,9 +1,6 @@
-from pydantic import BaseModel, Field
-
-
-class VirtualMachine(BaseModel):
-    name: str = Field(min_length=1, strip_whitespace=True)
-    ram: float = Field(gt=0)
-    cpu: float = Field(gt=0)
-    storage: float = Field(gt=0)
-    os: str = Field(pattern=r"^(windows|linux|win|lin|w|l)$")
+class VirtualMachine:
+    name: str
+    ram: float
+    cpu: float
+    storage: float
+    os: str
